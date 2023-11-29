03c3bb3030998badda687cbf16789a950212762f
Richard Harrah <topplethenunnery@gmail.com>
2023-11-28 20:03:15 -0500

    add error details

--- src/components/RouteErrorBoundary.tsx
+++ src/components/RouteErrorBoundary.tsx
@@ -1,3 +1,19 @@
+import { useRouteError } from "react-router-dom";
+
 export function RouteErrorBoundary() {
+  const error = useRouteError();
+
+  if (error instanceof Error) {
+    return (
+      <div>
+        <p>{error.message}</p>
+        <pre
+          style={{ color: "red", backgroundColor: "rgba(255, 255, 255, 0.8)" }}
+        >
+          {error.stack}
+        </pre>
+      </div>
+    );
+  }
   return <div>Error in router</div>;
 }
